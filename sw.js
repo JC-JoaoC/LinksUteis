c39b5127a9d338502400553bc321d188ec4e651a
unknown <joaoc0963@gmail.com>
2026-07-08 18:16:53 -0300

    add new links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'links-wfs-v2';
+const CACHE_NAME = 'links-wfs-v4';
 const ASSETS = [
     './',
     './index.html',
@@ -10,6 +10,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
@@ -19,7 +20,7 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((keys) =>
             Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-        )
+        ).then(() => self.clients.claim())
     );
 });
 
